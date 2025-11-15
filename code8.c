ab0d70c09a99ea0f60a9d5f2163e8d042d803b5e
hritikShrivastava931 <hritikshrivastava931@gmail.com>
2025-11-15 15:34:41 +0530

    Fix formatting and remove unnecessary lines

--- code8.c
+++ code8.c
@@ -2,16 +2,13 @@
 
 int main() {
     int n, sum = 0, i;
-
-    // Input value of n
     printf("Enter a positive integer: ");
     scanf("%d", &n);
-
-    // Calculate sum using loop
     for(i = 1; i <= n; i++) {
         sum = sum + i;
     }
     printf("The sum of first %d natural numbers is: %d\n", n, sum);
     
     return 0;
+
 }
