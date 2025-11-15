94d0f1979c2682e6c91bf4bc4a44e9c51d165ea3
hritikShrivastava931 <hritikshrivastava931@gmail.com>
2025-11-15 15:35:36 +0530

    Fix formatting and remove unnecessary comments

--- code1.c
+++ code1.c
@@ -4,14 +4,10 @@ int main() {
     int a,b,sum;
     printf("Enter first number: ");
     scanf("%d",&a);
-
     printf("Enter second number: ");
     scanf("%d",&b);
-
-    // Calculating sum
     sum = a + b;
-
-    // Displaying result
     printf("The sum of %d and %d is: %d\n",a,b,sum);
     return 0;
+
 }
