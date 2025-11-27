f5bc36e36f2bc156f6d12709b8bfbf3211195092
hritikShrivastava931 <hritikshrivastava931@gmail.com>
2025-11-27 21:51:10 +0530

    Add files via upload

---
+++ code87.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j, distinct = 1;
+
+    printf("Enter the size of the square matrix: ");
+    scanf("%d", &n);
+
+    int matrix[n][n];
+
+    printf("Enter the elements of the matrix:\n");
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    for (i = 0; i < n; i++) {
+        for (j = i + 1; j < n; j++) {
+            if (matrix[i][i] == matrix[j][j]) {
+                distinct = 0;
+                break;
+            }
+        }
+        if (!distinct) break;
+    }
+
+    if (distinct)
+        printf("Diagonal elements are distinct.\n");
+    else
+        printf("Diagonal elements are not distinct.\n");
+
+    return 0;
+}
